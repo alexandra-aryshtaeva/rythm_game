4fe0191a8c8fcd678b8952297c53a50ffd5be8de
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2024-01-08 17:30:17 +0000

    some clicking on the button works

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 // Game start:
 // Music start
 // animation start
-const button = document.querySelector("buton");
 
-function gameStart() {
-  const key = document.querySelector(`div[data-key"${e.code}"]`);
-  key.style.color = "rotate()20deg";
-  //   document.querySelector("audio")
-  //   music.play()
+// function gameStart() {
+//   // let key = document.querySelector(`div[data-key"${e.code}"]`);
+//   // let key = document.getElementById("key");
+//   // key.style.color = "blue";
+//   //   document.querySelector("audio")
+//   //   music.play()
+// }
+
+let button = document.querySelector("button");
+button.addEventListener("click", myFunction);
+
+function myFunction() {
+  let key = document.getElementById("key");
 }
 
-button.addEventListener("click", gameStart());
 
+Sound comnig out as the button is pressed
 function pressKey(e) {
   const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-
+// 
   if (!audio) return;
-
+// 
   audio.currentTime = 0;
   audio.play();
   console.log(e.key);
 }
 window.addEventListener("keydown", pressKey);
+// 
